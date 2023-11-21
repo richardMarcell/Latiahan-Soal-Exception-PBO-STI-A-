89c191b5b13163272f57ca8699697fdd6c72c895
Richard Marcell <richard.marcell8888@gmail.com>
2023-11-21 10:51:20 +0700

    implement template for task number 3

--- Soal Nomor 3/src/App.java
+++ Soal Nomor 3/src/App.java
@@ -1,5 +1,32 @@
 public class App {
+
+
+    protected String fileName;
+
+
+
+    static void bacaFile(String fileName) throws Exception
+    {
+        String[] fileLists = {"File 1", "File 2", "File 3", "File 4", "File 5"};
+
+        for(int i = 0; i < fileLists.length; i++) {
+            if(fileName == fileLists[i]) {
+                System.out.println("Anda Sudah Membaca File dengan nama: " + fileName);
+            } else if(fileName == null) {
+                throw new Exception("Terjadi Kesalahan!! Anda memberikan Nilai berupa Null");
+            } else {
+                
+            }
+        }
+    }
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        try {
+            bacaFile("File 1");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
